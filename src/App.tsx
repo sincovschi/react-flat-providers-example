00dce932fd32be3ff2dc119979597c1be3a36f21
Eugene <0ggy.sin@gmail.com>
2021-12-27 18:48:47 +0200

    feat(app): add raw providers

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useString } from "./providers/string-provider";
+import { useNumber } from "./providers/number-provider";
+import { useBoolean } from "./providers/boolean-provider";
+
+export function App() {
+  const number = useNumber();
+  const boolean = useBoolean();
+  const string = useString();
+  return (
+    <div>
+      <header>
+        <h3>React flat providers example</h3>
+        <h4>
+          <a href="https://github.com/sincovschi/react-flat-providers">
+            https://github.com/sincovschi/react-flat-providers
+          </a>
+        </h4>
+      </header>
+      <main>
+        <ul>
+          <li>useNumber(): {number}</li>
+          <li>useBoolean(): {boolean ? "true" : "false"}</li>
+          <li>useString(): {string}</li>
+        </ul>
+      </main>
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './App';
+import { NumberProvider } from './providers/number-provider';
+import { BooleanProvider } from './providers/boolean-provider';
+import { StringContext } from './providers/string-provider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello, Flat Providers</h1>
+    <NumberProvider>
+      <BooleanProvider initialValue={true}>
+        <StringContext.Provider value={'String'}>
+          <App />
+        </StringContext.Provider>
+      </BooleanProvider>
+    </NumberProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
